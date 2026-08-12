281d4474564348e42ca4719dac8034ce8001fcd3
Alireza Keshavarz <alireza.keshavarz.d@gmail.com>
2026-08-12 21:27:37 +0330

    chore: move common constexpr vars and functions to a header file

---
+++ src/common.h
@@ -0,0 +1,18 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <filesystem>
+#include <functional>
+
+namespace fs = std::filesystem;
+
+constexpr auto read_perm = fs::perms::others_read | fs::perms::group_read | fs::perms::owner_read;
+constexpr auto exec_perm = fs::perms::others_exec | fs::perms::group_exec | fs::perms::owner_exec;
+
+using CommandFunction = std::function<void(const std::vector<std::string_view> &)>;
+
+constexpr bool has_exec_perm(const fs::perms &perms) { return fs::perms::none != (perms & exec_perm); }
+
+constexpr bool has_read_perm(const fs::perms &perms) { return fs::perms::none != (perms & read_perm); }
+
+#endif // COMMON_H
