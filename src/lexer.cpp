d947cd4953ba23babdb20db2a28f1aaea9b81c8c
Alireza Keshavarz <alireza.keshavarz.d@gmail.com>
2026-08-12 21:28:35 +0330

    chore(formatting)

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -4,13 +4,12 @@
 #include <string_view>
 #include <vector>
 
-std::vector<std::string_view> split(const std::string_view &line,
-                                    char delimiter) {
-  std::vector<std::string_view> result;
-  for (auto subrange : line | std::views::split(delimiter)) {
-    result.emplace_back(subrange);
-  }
-  return result;
+std::vector<std::string_view> split(const std::string_view &line, char delimiter) {
+    std::vector<std::string_view> result;
+    for (auto subrange : line | std::views::split(delimiter)) {
+        result.emplace_back(subrange);
+    }
+    return result;
 }
 
 auto lex(const std::string_view &line) { return split(line, ' '); }
--- src/lexer.h
+++ src/lexer.h
@@ -3,7 +3,6 @@
 #include <string_view>
 #include <vector>
 
-std::vector<std::string_view> split(const std::string_view &line,
-                                    char delimiter);
+std::vector<std::string_view> split(const std::string_view &line, char delimiter);
 
 auto lex(const std::string_view &line);
